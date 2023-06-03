241c1fbf90589f1d6e172e655105d51356fcf6ba
liamhaller <liamhllr2@gmail.com>
2023-06-03 16:26:01 +0200

    e

--- R/structure_of_answers.R
+++ R/structure_of_answers.R
@@ -79,34 +79,34 @@ structure_categories <- function(dataframe, category_threshold = 20){
   names(categorical_variables) <-  category_name
 
 
-  ############# Now return list of answer options sorted by column ######
+  return(categorical_variables)
 
-  generate_unique_level_list(dataframe, categorical_variables)
+
+}
+
+
+
+#' Get structure of answer levels
+#'
+#' @param dataframe .
+#'
+#' @return .
+#' @export
+#'
+struct_level <- function(dataframe){
+
+  categories <- structure_categories(dataframe)
 
   final_list <- list()
-  for(i in 1:length(categorical_variables)){
+  for(i in 1:length(categories)){
 
-    final_list[[i]] <-  generate_unique_level_list(panel, categorical_variables[[i]])
+    final_list[[i]] <-  generate_unique_level_list(dataframe, categorical_variables[[i]])
 
   }
 
   return(final_list)
 
-}
 
-
-
-
-
-#tranform a tibble column to an unnamed vector
-#' Title
-#'
-#' @param tibblecolumn .
-#'
-#' @return .
-#'
-untibbled <- function(tibblecolumn){
-  unlist(unname(as.vector(tibblecolumn)))
 }
 
 
@@ -114,64 +114,3 @@ untibbled <- function(tibblecolumn){
 
 
 
-#' Generate unique level list
-#'
-#' @param dataframe .
-#' @param column_names .
-#' @importFrom purrr map_df
-#' @importFrom rlang enexpr
-#' @return .
-#'
-generate_unique_level_list <- function(dataframe, column_names){
-
-  total_vars <- length(column_names)
-
-  if (total_vars == 1) {
-
-
-    answer_levels <- dataframe[,match(column_names, colnames(dataframe))] %>% unique %>% sort
-
-    output <- data.frame(answer_levels)
-    colnames(output) <- rlang::enexpr(column_names)
-    unique_levels <- list()
-    unique_levels[[1]] <- output
-
-  } else {
-
-    answer_levels <- dataframe[,match(column_names, colnames(dataframe))] %>% purrr::map_df(unique) %>% purrr::map_df(sort)
-    unique_answer_levels <- answer_levels[!duplicated(as.list(answer_levels))]
-
-    no_unique_levels <- NCOL(unique_answer_levels)
-
-    #if there is only one answer category, the for loop will fail, so here we
-    #handle that case by itself
-
-    unique_levels <- list()
-    for(i in 1:no_unique_levels){
-
-      #Does the answer levels in the table(answer_levels) which contains all possible for each grou
-      #match the uniqe level that is rotated by the forloop, if yes it should be included in the list
-      string_match <- purrr::map_df(answer_levels, function(x) {
-        dezim:::stringmatch(check_string = untibbled(unique_answer_levels[,i]), untibbled(x))
-      })
-
-      #a boolean vector of which columns matched the given string
-      string_match <- untibbled(string_match)
-      #the column names of the columns that matched the output
-      matched_columns <- colnames(answer_levels[,string_match])
-
-      df_name <- untibbled(unique_answer_levels[,i])
-      #print(stringr::str_c(rlang::enexpr(df_name), collapse = ", "))
-      output <- as.data.frame(matched_columns)
-      colnames(output) <- stringr::str_c(rlang::enexpr(df_name), collapse = ",")
-
-      unique_levels[[i]] <- output
-    }
-    #Now there is a list where each name is one of the unique levels
-    #if a column's answer matches the answer's levels include append it to that list
-
-
-  }
-
-  return(unique_levels)
-}
