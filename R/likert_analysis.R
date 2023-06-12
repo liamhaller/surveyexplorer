7fbbf08b48b4a126f3fd0ba31ec1f504b80589ee
liamhaller <liamhllr2@gmail.com>
2023-06-12 17:26:17 +0200

    likely to fail

--- R/likert_analysis.R
+++ R/likert_analysis.R
@@ -21,7 +21,7 @@ likert_summary <- function(data, high_to_low = TRUE, order_rows = FALSE){
 
   if(class(data) != "data.frame"){
     dataframe <- as.data.frame(data)
-    colnames(dataframe) <- paste0("'",(substitute(data)),"'")
+    colnames(dataframe) <- c(quote(data))
 
   } else {
     dataframe <- data
